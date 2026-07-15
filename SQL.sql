747c1d6a2fbf945fa674b01273f92f0c690c8bcb
Melbin Varghese <melbinvarghese77@gmail.com>
2026-07-15 16:10:52 +0530

    urls

--- SQL.sql
+++ SQL.sql
@@ -3,3 +3,8 @@ use careermomentum;
 show tables;
 
 select * from employers;
+select * from jobs;
+select * from applications;
+select * from employers;
+select * from seeker_profiles;
+select * from seekers;
